a161d23c43ffc3ded7c2defca16456c0f0cb396b
suadbadusha <67995271+suadbadusha@users.noreply.github.com>
2021-10-08 21:07:17 +0530

    Update urls.py

--- badusha/urls.py
+++ badusha/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-   # path('',include('travello.urls')),
     path('',include('calc.urls')),
     path('admin/', admin.site.urls),
    
