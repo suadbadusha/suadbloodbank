2e36e0d75edfd8cd6f389502d433e2126830d040
suadbadusha <67995271+suadbadusha@users.noreply.github.com>
2021-10-08 21:07:51 +0530

    Delete travello directory

--- travello/views.py
+++
@@ -1,9 +0,0 @@
-from travello.models import Destination
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-
-   dests =Destination.objects.all()
-    
-   return render(request,'index.html', {'dests' : dests})
